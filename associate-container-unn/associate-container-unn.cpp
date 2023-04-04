ed11088976ad13095a7a500277fc00efb4634052
Dmitry <xtopz1337@gmail.com>
2023-04-04 17:59:38 +0300

    Добавьте файлы проекта.

---
+++ associate-container-unn/associate-container-unn.cpp
@@ -0,0 +1,12 @@
+﻿// associate-container-unn.cpp: определяет точку входа для приложения.
+//
+
+#include "associate-container-unn.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
