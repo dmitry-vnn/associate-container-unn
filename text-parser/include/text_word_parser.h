6326e1d4e7364f7b1ed999575cd063bb6f677ed1
Дмитрий <@DESKTOP-8D551CR>
2023-05-03 23:48:33 +0300

    Add: TextWordParser class

---
+++ text-parser/include/text_word_parser.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include <iostream>
+#include <vector>
+#include <functional>
+
+class TextWordParser
+{
+	
+private:
+	using WordHandler = std::function<void(std::wstring)>;
+	using WordPredicate = std::function<bool(std::wstring)>;
+
+private:
+	std::string _filePath;
+	WordPredicate _wordFilter;
+	char _wordSplitter;
+
+public:
+	TextWordParser(
+		std::string filePath,
+		WordPredicate wordFilter,
+		char wordSplitter
+	):
+		_filePath(std::move(filePath)),
+		_wordFilter(std::move(wordFilter)),
+		_wordSplitter(wordSplitter) {}
+};
---
+++ text-parser/src/text_word_parser.cpp
@@ -0,0 +1,1 @@
+﻿#include "text_word_parser.h"
