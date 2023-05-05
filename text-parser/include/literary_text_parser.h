ca34a09ea8e353ccc2e00e0534791c236cab51ab
Дмитрий <@DESKTOP-8D551CR>
2023-05-05 16:37:38 +0300

    Edit: done

--- text-parser/include/literary_text_parser.h
+++ text-parser/include/literary_text_parser.h
@@ -1,6 +1,24 @@
 ﻿#pragma once
 
+#include <functional>
+#include <string>
+
+#include "text_word_parser.h"
+
 class LiteraryTextParser
 {
-	public:
+
+private:
+	using WordHandler = TextWordParser::WordHandler;
+
+private:
+	TextWordParser* _parser;
+
+public:
+	LiteraryTextParser(std::string filePath, WordHandler wordHandler);
+	~LiteraryTextParser();
+
+
+public:
+	void Parse() const { _parser->Parse(); }
 };
--- text-parser/src/literary_text_parser.cpp
+++ text-parser/src/literary_text_parser.cpp
@@ -1,1 +1,27 @@
 ﻿#include "literary_text_parser.h"
+
+LiteraryTextParser::LiteraryTextParser(std::string filePath, WordHandler wordHandler): _parser(nullptr)
+{
+	std::function<bool(wchar_t)> charFilter = [](wchar_t c)
+	{
+		return
+			c == L'\''
+			|| c >= L'а' && c <= L'я'
+			|| c >= L'А' && c <= L'Я'
+			|| c >= L'a' && c <= L'z'
+			|| c >= L'A' && c <= L'Z'
+			|| c >= L'0' && c <= L'9';
+	};
+
+	std::vector<auto> splitters = {L' '};
+
+	_parser = new auto(
+		std::move(filePath), std::move(charFilter),
+		std::move(wordHandler), std::move(splitters)
+	);
+}
+
+LiteraryTextParser::~LiteraryTextParser()
+{
+	delete _parser;
+}
