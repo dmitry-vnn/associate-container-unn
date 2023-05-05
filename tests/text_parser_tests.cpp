58d01329a3dda5ed5484a081ef58497c6626e053
Дмитрий <@DESKTOP-8D551CR>
2023-05-05 20:12:11 +0300

    Edit: add test

--- tests/text_parser_tests.cpp
+++ tests/text_parser_tests.cpp
@@ -1,13 +1,24 @@
 #include <codecvt>
 #include <gtest/gtest.h>
 
+#include "hash_table.h"
+#include "literary_text_parser.h"
+#include "ordered_table.h"
+#include "scan_table.h"
 #include "text_word_parser.h"
+
+void Localize()
+{
+	std::locale system("");
+	std::locale::global(system);
+}
+
+std::string filePath = R"(..\..\..\text.txt)";
+
 
 TEST(TextParser, test_file_input)
 {
-
-	std::locale system("");
-	std::locale::global(system);
+	Localize();
 
 	std::function predicate =
 		[] (wchar_t) { return true; };
@@ -21,8 +32,28 @@ TEST(TextParser, test_file_input)
 	std::vector splitters = { L' ', L'\n', L'\0'};
 
 	TextWordParser parser(
-		R"(..\..\..\text.txt)", 
+		filePath, 
 		predicate, handler,
 		std::move(splitters)
 	);
+}
+
+TEST(LiteraryTextParser, word_handling)
+{
+	Localize();
+
+
+	size_t count = 0;
+
+	std::function handler = [&](const std::wstring& word)
+	{
+		count++;
+	};
+
+	LiteraryTextParser parser(filePath, std::move(handler));
+
+	parser.Parse();
+
+	EXPECT_EQ(114140, count);
+
 }
