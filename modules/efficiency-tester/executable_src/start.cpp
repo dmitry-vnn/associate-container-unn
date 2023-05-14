10dfa9591dede4cdaf7b878285d7accbc822ba1d
Дмитрий <@DESKTOP-8D551CR>
2023-05-14 13:12:02 +0300

    Edit: print efficiency test info

--- modules/efficiency-tester/executable_src/start.cpp
+++ modules/efficiency-tester/executable_src/start.cpp
@@ -1,7 +1,71 @@
+#include <iomanip>
+
 #include "tables_efficiency_tester.h"
+
 
 int main()
 {
-	TablesEfficiencyTester().DoIt();
+
+	TablesEfficiencyTester tester;
+
+	tester.Test();
+
+	auto totalStatistic = tester.GetStatistic();
+
+	printf(
+		"parsing file time (s): %.3f\n",
+
+		tester.GetParsingWordsFromFileDurationSeconds()
+	);
+
+	printf("tables info:\n");
+	printf(
+		"%-15s%-15s%-15s%-15s%-15s\n",
+
+		"TABLE",
+		"ADD TIME (s)",
+		"FIND TIME",
+		"REMOVE TIME",
+		"RECORDS"
+	);
+
+	printf("%s\n", std::string(15 * 5, '-').c_str());
+
+	for (const auto & record : totalStatistic)
+	{
+		auto tableType = record.first;
+		auto& statistic = record.second;
+
+		std::string tableName;
+		switch (tableType)
+		{
+			case TableType::SCAN_TABLE:
+				tableName = "ScanTable";
+				break;
+
+			case TableType::ORDERED_TABLE:
+				tableName = "OrderedTable";
+				break;
+
+			case TableType::HASH_TABLE:
+				tableName = "HashTable";
+				break;
+
+			case TableType::TREE_TABLE:
+				tableName = "TreeTable";
+				break;
+		}
+
+		printf(
+			"%-15s%-15.3f%-15.3f%-15.3f%-15llu\n",
+			
+			tableName.c_str(),
+			statistic.addRecordsTotalDurationTimeMillis,
+			statistic.findRecordTotalDurationTimeMillis,
+			statistic.removeRecordsTotalDurationTimeMillis,
+			statistic.totalRecordsCount
+		);
+	}
+
 	return 0;
 }
