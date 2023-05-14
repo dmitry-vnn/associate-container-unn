29678eed607d436e97663db4a7084eef3308d491
Дмитрий <@DESKTOP-8D551CR>
2023-05-14 13:10:42 +0300

    Add: TablesEfficiencyTester

--- modules/efficiency-tester/include/tables_efficiency_tester.h
+++ modules/efficiency-tester/include/tables_efficiency_tester.h
@@ -1,9 +1,70 @@
 ﻿#pragma once
 
+#include <iostream>
+#include <map>
+#include <vector>
+
+#include "table.h"
+
+struct TableStatistic
+{
+	double addRecordsTotalDurationTimeMillis = 0;
+	double removeRecordsTotalDurationTimeMillis = 0;
+	double findRecordTotalDurationTimeMillis = 0;
+
+	size_t totalRecordsCount = 0;
+
+	TableStatistic() = default;
+};
+
+enum class TableType
+{
+
+	SCAN_TABLE,
+	ORDERED_TABLE,
+	HASH_TABLE,
+	TREE_TABLE
+
+
+};
+
 class TablesEfficiencyTester
 {
 
-public:
-	void DoIt();
+private:
+	std::vector<std::wstring> _parsedWords;
 
+private:
+	std::map<TableType, TableStatistic> _statistic;
+
+private:
+	double _parsingWordsFromFileDurationSeconds;
+
+public:
+	TablesEfficiencyTester() = default;
+
+	void Test();
+
+private:
+	void Parse();
+
+	void TestTable(
+		const std::pair<
+			const TableType, 
+			std::unique_ptr<Table<std::wstring, size_t>>
+		>& record);
+
+	void TestTables();
+
+public:
+
+	std::map<TableType, TableStatistic> GetStatistic() const
+	{
+		return _statistic;
+	}
+
+	double GetParsingWordsFromFileDurationSeconds() const
+	{
+		return _parsingWordsFromFileDurationSeconds;
+	}
 };
--- modules/efficiency-tester/src/tables_efficiency_tester.cpp
+++ modules/efficiency-tester/src/tables_efficiency_tester.cpp
@@ -1,10 +1,153 @@
 ﻿#include "tables_efficiency_tester.h"
 
 #include "hash_table.h"
+#include "literary_text_parser.h"
 
-void TablesEfficiencyTester::DoIt()
+#include <chrono>
+
+#include "ordered_table.h"
+#include "scan_table.h"
+
+using Time = std::chrono::time_point<std::chrono::system_clock>;
+
+Time TimeNow()
 {
-	HashTable<int, int> table(1);
+	return std::chrono::system_clock::now();
+}
 
-	table.Print();
+double DurationTimeSeconds(const Time& t1, const Time& t2)
+{
+	std::chrono::duration
+		<double, std::chrono::seconds::period>
+
+	duration = t2 - t1;
+
+	return duration.count();
+}
+
+void TablesEfficiencyTester::Parse()
+{
+	auto startTime = TimeNow();
+
+	LiteraryTextParser parser(
+		R"(..\..\..\..\..\text.txt)", 
+		[this] (const std::wstring& word)
+		{
+			_parsedWords.push_back(word);
+		}
+	);
+
+	parser.Parse();
+
+	auto endTime = TimeNow();
+
+	_parsingWordsFromFileDurationSeconds = 
+		DurationTimeSeconds(startTime, endTime);
+}
+
+void TablesEfficiencyTester::TestTable(
+	const std::pair<const TableType, std::unique_ptr<Table<std::wstring, size_t>>>& record)
+{
+
+	auto& statistic = _statistic[record.first];
+	auto& table = record.second;
+
+
+	auto startTime = TimeNow();
+
+	for (const auto & word : _parsedWords)
+	{
+		auto iterator = table->Find(word);
+		if (iterator == table->End())
+		{
+			table->Add(word, 1);
+		} else
+		{
+			iterator->value++;
+		}
+	}
+
+	auto endTime = TimeNow();
+
+	statistic.addRecordsTotalDurationTimeMillis = 
+		DurationTimeSeconds(startTime, endTime);
+
+	startTime = TimeNow();
+
+
+	statistic.totalRecordsCount = table->Size();
+
+
+	for (const auto& word : _parsedWords)
+	{
+		auto iterator = table->Find(word);
+	}
+
+	endTime = TimeNow();
+
+	statistic.findRecordTotalDurationTimeMillis =
+		DurationTimeSeconds(startTime, endTime);
+
+	startTime = TimeNow();
+
+	for (const auto & word : _parsedWords)
+	{
+		table->Remove(word);
+	}
+
+	endTime = TimeNow();
+
+	statistic.removeRecordsTotalDurationTimeMillis =
+		DurationTimeSeconds(startTime, endTime);
+
+}
+
+void TablesEfficiencyTester::TestTables()
+{
+
+	using K = std::wstring;
+	using V = size_t;
+
+	std::map<
+		TableType,
+		std::unique_ptr<Table<K, V>>
+	> tables;
+
+	/*tables.insert({
+		TableType::SCAN_TABLE,
+		std::make_unique<ScanTable<K, V>>()
+	});
+
+	tables.insert({
+		TableType::ORDERED_TABLE,
+		std::make_unique<OrderedTable<K, V>>()
+	});*/
+
+	tables.insert({
+		TableType::HASH_TABLE,
+		std::make_unique<HashTable<K, V, true>>()
+	});
+
+
+	auto iterator = tables.begin();
+
+	while (iterator != tables.end())
+	{
+		_statistic.insert({iterator->first, {}});
+
+		++iterator;
+	}
+
+
+	for (auto& record : tables)
+	{
+		TestTable(record);
+	}
+}
+
+void TablesEfficiencyTester::Test()
+{
+	Parse();
+
+	TestTables();
 }
